434b21875c7911a4aabdcd47eab99c03da230e43
Jun Zhu <zhujun981661@gmail.com>
2017-06-05 09:49:34 +0200

    Implement the random selection algorithm

--- sorting_and_selection/src/main.cpp
+++ sorting_and_selection/src/main.cpp
@@ -9,6 +9,7 @@
 #include <time.h>
 
 #include "sorting.h"
+#include "selection.h"
 
 
 int main()
@@ -34,29 +35,51 @@ int main()
   unsigned long long test_length = numbers.end() - numbers.begin();
 
   // test the sort algorithm implemented in <algorithm> library
-  std::vector<int> numbers_cp = numbers;
+  std::vector<int> numbers_cp1 = numbers;
   clock_t t0 = clock();
-  std::sort(numbers_cp.begin(), numbers_cp.begin() + test_length);
+  std::sort(numbers_cp1.begin(), numbers_cp1.begin() + test_length);
+  std::cout << "-------------------------" << std::endl;
   std::cout << "Using default sort algorithm:" << std::endl;
   std::cout << "Elapse time: " << 1.0*(clock() - t0)/CLOCKS_PER_SEC << std::endl;
 
-  // test my quicksort algorithm
+  // test my sorting algorithm
+  std::vector<int> numbers_cp2 = numbers;
   t0 = clock();
-  long long total_comparison = quicksort(numbers.begin(), numbers.begin() + test_length);
-  std::cout << "Testing my quicksort algorithm" << std::endl;
+  long long total_comparison = quicksort(
+      numbers_cp2.begin(), numbers_cp2.begin() + test_length);
+  std::cout << "-------------------------" << std::endl;
+  std::cout << "Testing my sorting algorithm" << std::endl;
   std::cout << "Estimated total comparisons: " << total_comparison << std::endl;
   std::cout << "Elapse time: " << 1.0*(clock() - t0)/CLOCKS_PER_SEC << std::endl;
 
-  bool ok = true;
+  bool sorting_ok = true;
   for ( int i=0; i< test_length; ++i) {
-//    std::cout << numbers_cp[i] << " " << numbers[i] << std::endl;
-    if ( numbers_cp[i] != numbers[i] ) { ok = false; }
+//    std::cout << numbers_cp1[i] << " " << numbers_cp2[i] << std::endl;
+    if ( numbers_cp1[i] != numbers_cp2[i] ) { sorting_ok = false; }
   }
 
-  if (ok) {
-    std::cout << "correct implementation!" << std::endl;
+  if (sorting_ok) {
+    std::cout << "correct implementation of sorting!" << std::endl;
   } else {
-    std::cout << "wrong implementation!" << std::endl;
+    std::cout << "incorrect implementation of sorting!" << std::endl;
+  }
+
+  // test my selection algorithm
+  t0 = clock();
+  long unsigned n = 709;
+  std::vector<int>::iterator selected = randomSelect(numbers.begin(), numbers.end(), n);
+  std::cout << "-------------------------" << std::endl;
+  std::cout << "Testing my selection algorithm" << std::endl;
+  std::cout << "Elapse time: " << 1.0*(clock() - t0)/CLOCKS_PER_SEC << std::endl;
+
+  bool selection_ok;
+  std::cout << numbers_cp1[n-1] << " " << *selected << std::endl;
+  if ( numbers_cp1[n-1] == *selected ) {
+    selection_ok = true;
+    std::cout << "correct implementation of selection!" << std::endl;
+  } else {
+    selection_ok = false;
+    std::cout << "incorrect implementation of selection!" << std::endl;
   }
 
 }
--- sorting_and_selection/src/selection.h
+++ sorting_and_selection/src/selection.h
@@ -5,4 +5,56 @@
 #ifndef SORTING_AND_SELECTION_SELECTION_H
 #define SORTING_AND_SELECTION_SELECTION_H
 
+
+#include <iostream>
+#include <random>
+
+#include "utilities.h"
+
+
+//
+// implementation of "random selection" method
+// Select the nth biggest element in the range [first, last).
+//
+//
+template <class Iterator>
+inline Iterator randomSelect(Iterator first, Iterator last, long unsigned n) {
+
+  if ( n > last - first ) {
+    throw std::invalid_argument("n is larger than the length of the array!");
+  }
+
+  // the following two conditions ensured that the length of the array is
+  // bigger than 2.
+  if ( n == 0 ) { return first; }
+  if ( n == last - first ) { return last - 1; }
+
+  Iterator pivot;
+
+  // randomly pick the pivot
+
+  std::random_device rd;
+
+  /* Random number generator */
+  std::default_random_engine generator(rd());
+
+  /* Distribution on which to apply the generator */
+  std::uniform_int_distribution<long long unsigned> distribution(0, last - first - 1);
+
+  long long unsigned random_pivot = distribution(generator);
+  pivot = first + random_pivot;
+
+  // applying partition
+  pivot = myPartition(first, last, pivot);
+
+  if ( pivot - first == n - 1 ) {
+    return pivot;
+  } else if ( pivot - first > n - 1) {
+    return randomSelect(first, pivot, n);
+  } else {
+    return randomSelect(pivot + 1, last, n - (pivot - first + 1));
+  }
+
+}
+
 #endif // SORTING_AND_SELECTION_SELECTION_H
