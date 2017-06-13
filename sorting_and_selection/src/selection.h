ff746c07db5864f6f582a9ecd75d5cd1cd781de0
Jun Zhu <zhujun981661@gmail.com>
2017-06-14 00:18:19 +0200

    Implement non-inplace selection.
    Fix the bug when n=1

--- sorting_and_selection/src/selection.h
+++ sorting_and_selection/src/selection.h
@@ -10,26 +10,33 @@
 
 #include <iostream>
 #include <random>
+#include <vector>
+#include <iterator>
 
 #include "utilities.h"
 
 
 //
 // implementation of "random selection" method
-// Select the nth biggest element in the range [first, last).
+// Select the nth smallest element in the range [first, last).
+// Note:: the original container will be sorted.
 //
+// @param first: iterator to the first position
+// @param last: iterator to the last position
+// @param n: the rank of the number (smallest)
+//
+// @return: iterator to the selected number
 //
 template <class Iterator>
-inline Iterator rSelect(Iterator first, Iterator last, long unsigned n) {
+inline Iterator rSelect_inplace(Iterator first, Iterator last, int unsigned n) {
 
-  if ( n > last - first ) {
-    throw std::invalid_argument("n is larger than the length of the array!");
+  if ( n < 1 || n > last - first ) {
+    throw std::invalid_argument("Out of range: n!");
   }
 
   // the following two conditions ensured that the length of the array is
   // bigger than 2.
-  if ( n == 0 || n == 1 ) { return first; }
-  if ( n == last - first ) { return last - 1; }
+  if ( n == 1 && last - first == 1 ) { return first; }
 
   Iterator pivot, boundary;
 
@@ -43,7 +50,7 @@ inline Iterator rSelect(Iterator first, Iterator last, long unsigned n) {
   /* Distribution on which to apply the generator */
   std::uniform_int_distribution<long long unsigned> distribution(0, last - first - 1);
 
-  long long unsigned random_pivot = distribution(generator);
+  long unsigned random_pivot = distribution(generator);
   pivot = first + random_pivot;
 
   // applying partition
@@ -57,11 +64,31 @@ inline Iterator rSelect(Iterator first, Iterator last, long unsigned n) {
     // is not guaranteed to be the largest number int the array [first, boundary).
     // If it happens that "boundary - first - 1 == n - 1", the algorithm will return
     // "boundary - 1" if "boundary" were used as the second argument.
-    return rSelect(first, boundary + 1, n);
+    return rSelect_inplace(first, boundary + 1, n);
   } else {
-    return rSelect(boundary, last, n - (boundary - first));
+    return rSelect_inplace(boundary, last, n - (boundary - first));
   }
 
+}
+
+
+//
+// implementation of "random selection" method
+// Select the nth smallest element in the range [first, last).
+//
+// @param first: iterator to the first position
+// @param last: iterator to the last position
+// @param n: the rank of the number (smallest)
+//
+// @return: iterator to the selected number
+//
+template <class Iterator>
+inline Iterator rSelect(Iterator first, Iterator last, int unsigned n) {
+
+  // copy the elements between first and last in the original container into a vector
+  std::vector<typename std::iterator_traits<Iterator>::value_type> data(first, last);
+
+  return rSelect_inplace(data.begin(), data.end(), n);
 }
 
 #endif // SORTING_AND_SELECTION_SELECTION_H
