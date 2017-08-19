7173bfdb0eaed7ead961064e12bef72db339ba8e
Jun Zhu <zhujun981661@gmail.com>
2017-08-19 19:54:02 +0200

    Comments

--- graph/src/graph_algorithms/dijkstra.h
+++ graph/src/graph_algorithms/dijkstra.h
@@ -25,6 +25,8 @@
 // std::set keeps tracking the smallest element so that the look-up
 // time is only O(1).
 //
+// The time complexity is O(VlogV + V + 2ElogV) = O(ElogV)
+//
 // The speed is comparable with the implementation using priority queue.
 // However, the overhead (memory) of a self-balanced RB-tree is much higher.
 //
@@ -38,7 +40,7 @@
 //
 // @param return: a vector containing a pair of the shortest distance
 //                between each vertex to the source as well as the
-//                parent vertex of each vertex.
+//                parent vertex of each vertex in the shortest path.
 //
 template <class G, class T>
 std::vector<std::pair<double, T>> dijkstra_base_tree(
@@ -54,10 +56,11 @@ std::vector<std::pair<double, T>> dijkstra_base_tree(
     return {};
   }
 
-  // A set of vertex <current shortest distance, index>
+  // A set of vertices <current shortest distance, destination index>
   std::set<std::pair<double, int>> remain;
   // Store the shortest distance and its parent vertex for each vertex
   std::vector<std::pair<double, T>> distances(graph.size());
+  // O(VlogV)
   for (int i = 0; i < graph.size(); ++i) {
     if (graph.indexToValue(i) != source) {
       remain.insert(std::make_pair(max_distance, i));
@@ -74,6 +77,7 @@ std::vector<std::pair<double, T>> dijkstra_base_tree(
     // Pick the index in the 'remain' set with the shortest distance.
     int selected_index = remain.begin()->second;
 
+    // O(1)*V = O(V), V is the outer loops
     remain.erase(remain.begin());
 
     if ( source != destination &&
@@ -84,23 +88,28 @@ std::vector<std::pair<double, T>> dijkstra_base_tree(
     // Loop the neighbors of the "selected_index" which is still in the
     // "remain" set.
     graph::Edge<T> *current_edge = graph.getVertexByIndex(selected_index)->next;
+    // Since each edge will only be visited once, so the total time
+    // complexity of the two loops is only O(E)
     while (current_edge) {
       int current_index = graph.valueToIndex(current_edge->value);
 
+      if ( current_edge->weight < 0 ) {
+        std::cout << "Graph has edge with negative weight!" << std::endl;
+        return {};
+      }
       // Update shortest distance information.
       // Note: It is not necessary to check whether 'currentIndex' has
       // been visited (here not in 'remain' container) since its
       // shortest distance will (should) not be updated further.
       // Moreover, the find operation is expensive compared to an
-      // addiction operation and a comparing.
-      if ( current_edge->weight < 0 ) {
-        throw std::invalid_argument("Graph has edge with negative weight!");
-      }
+      // addition operation and a comparing.
       double new_distance = distances[selected_index].first + current_edge->weight;
       if (distances[current_index].first > new_distance) {
+        // O(ElogV)
         remain.erase(std::make_pair(distances[current_index].first, current_index));
         distances[current_index].first = new_distance;
         distances[current_index].second = graph.indexToValue(selected_index);
+        // O(ElogV)
         remain.insert(std::make_pair(distances[current_index].first, current_index));
       }
 
@@ -112,8 +121,14 @@ std::vector<std::pair<double, T>> dijkstra_base_tree(
 }
 
 //
-// This is a similar implementation as dijkstra_base_tree excpet a
+// This is a similar implementation as dijkstra_base_tree except a
 // priority_queue is used to track the un-finished vertex set.
+//
+// Since there is decrease_key operation, the time complexity is
+// O(VlogV + E + ElogE) = O(ElogE) = O(ElogV), E <= V^2
+// If decrease_key is implemented, the time complexity will become
+// O(VlogV + V + 2ElogV) = O(ElogV), which is almost the same. But
+// the decrease_key operation has its overhead.
 //
 // The speed is comparable with the implementation using self-balanced
 // RB-tree.
@@ -137,7 +152,10 @@ std::vector<std::pair<double, T>> dijkstra_base_priority_queue(
   std::vector<distance_index_pair_t>,
   std::greater<distance_index_pair_t>> remain;
   // Store the shortest distance and its parent vertex for each vertex.
+  // The index in the vector is the same as the index of the vertex in
+  // the graph.
   std::vector<std::pair<double, T>> distances(graph.size());
+  // O(VlogV)
   for (int i = 0; i < graph.size(); ++i) {
     if (graph.indexToValue(i) != source) {
       remain.push(std::make_pair(max_distance, i));
@@ -154,6 +172,7 @@ std::vector<std::pair<double, T>> dijkstra_base_priority_queue(
     double selected_distance = remain.top().first;
     int selected_index = remain.top().second;
 
+    // O(1)*E = O(E), E is the outer loops
     remain.pop();
 
     if ( source != destination &&
@@ -171,25 +190,28 @@ std::vector<std::pair<double, T>> dijkstra_base_priority_queue(
     // Loop the neighbors of the "selected_index" which is still in the
     // "remain" set.
     graph::Edge<T> *current_edge = graph.getVertexByIndex(selected_index)->next;
+    // Since each edge will only be visited once, so the total time
+    // complexity of the two loops is only O(E)
     while (current_edge) {
       int current_index = graph.valueToIndex(current_edge->value);
 
+      if ( current_edge->weight < 0 ) {
+        throw std::invalid_argument("Graph has edge with negative weight!");
+      }
       // Update shortest distance information
       // Note: It is not necessary to check whether 'currentIndex' has
       // been visited (here not in 'remain' container) since its
       // shortest distance will (should) not be updated further.
       // Moreover, the find operation is expensive compared to an
-      // addiction operation and a comparing.
+      // addition operation and a comparing.
       //
       // Also, it is not necessary to find and remove the old
       // (distance, vertex) pair here since it will pop up later.
-      if ( current_edge->weight < 0 ) {
-        throw std::invalid_argument("Graph has edge with negative weight!");
-      }
       double new_distance = distances[selected_index].first + current_edge->weight;
       if (distances[current_index].first > new_distance) {
         distances[current_index].first = new_distance;
         distances[current_index].second = graph.indexToValue(selected_index);
+        // O(ElogE)
         remain.push(std::make_pair(distances[current_index].first, current_index));
       }
 
@@ -247,7 +269,6 @@ inline void showDijkstraPath(const G& graph,
     path.pop();
   }
   std::cout << destination << std::endl;
-
 };
 
 
