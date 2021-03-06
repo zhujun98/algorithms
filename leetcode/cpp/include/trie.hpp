5a67c534403deec434d3542dc07336c3e9e951fe
zhujun98 <zhujun981661@gmail.com>
2021-03-06 09:00:45 +0100

    Add Leetcode 208: Trie

---
+++ leetcode/cpp/include/trie.hpp
@@ -0,0 +1,31 @@
+//
+// Created by jun on 3/6/21.
+//
+
+#ifndef ALGORITHMS_TRIE_H
+#define ALGORITHMS_TRIE_H
+
+template<typename T>
+class Trie {
+
+public:
+
+  Trie() {
+
+  }
+
+  void insert(const T& word) {
+
+  }
+
+  bool search(const T& word) {
+
+  }
+
+  bool startsWith(const T& prefix) {
+
+  }
+};
+
+
+#endif //ALGORITHMS_TRIE_H
