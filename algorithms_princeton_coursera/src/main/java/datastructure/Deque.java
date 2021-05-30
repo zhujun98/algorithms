d20676b19ed1dbc3f0f55e4a6af48659305fa0cc
zhujun98 <zhujun981661@gmail.com>
2021-05-30 23:06:36 +0200

    Add template for Deque and RandomizedQueue

---
+++ algorithms_princeton_coursera/src/main/java/datastructure/Deque.java
@@ -0,0 +1,49 @@
+package datastructure;
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+}
