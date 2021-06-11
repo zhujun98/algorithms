e675a9d3de4c2faece7d040d939c43d6daefb827
zhujun98 <zhujun981661@gmail.com>
2021-06-11 08:11:01 +0200

    Implement in-order DFT search for 098

--- leetcode/cpp/include/098.hpp
+++ leetcode/cpp/include/098.hpp
@@ -16,17 +16,44 @@
 
 // Iterative traverse with valid range
 // Time complexity O(N), space complexity O(N)
-bool validate(TreeNode* root, TreeNode* low, TreeNode* high) {
-  if (root == nullptr) return true;
+bool validate(TreeNode* node, TreeNode* low, TreeNode* high) {
+  if (node == nullptr) return true;
 
-  if (low != nullptr && root->val <= low->val) return false;
-  if (high != nullptr && root->val >= high->val) return false;
+  if (low != nullptr && node->val <= low->val) return false;
+  if (high != nullptr && node->val >= high->val) return false;
 
-  return validate(root->left, low, root) && validate(root->right, root, high);
+  return validate(node->left, low, node) && validate(node->right, node, high);
 }
 
 bool isValidBST(TreeNode* root) {
   return validate(root, nullptr, nullptr);
 }
+
+// In order search: Left -> node -> right, we validate the current element should
+//                  be smaller than the next one
+class IsValidBSTInorder {
+  TreeNode* prev;
+
+  bool validateInorder(TreeNode* node) {
+    if (node == nullptr) return true;
+
+    if (!validateInorder(node->left)) return false;
+
+    if (prev != nullptr && node->val <= prev->val) return false;
+
+    // This is the tricky part! The node 'prev' is shared by all
+    // the recursively called functions. The 'prev' node is the left node of
+    // the current node, whenever a function on the current left-most
+    // node returns, the 'prev' will be set to this left-most node.
+    prev = node;
+    return validateInorder(node->right);
+  }
+
+public:
+  bool operator()(TreeNode* root) {
+    return validateInorder(root);
+  }
+};
+
 
 #endif //LEETCODE_ALGO_CPP_098_H
--- leetcode/python/098.py
+++ leetcode/python/098.py
@@ -3,7 +3,7 @@
 """
 
 
-def validate(self, node, low, high) -> bool:
+def validate(node, low, high) -> bool:
     if node is None:
         return True
 
@@ -12,9 +12,29 @@ def validate(self, node, low, high) -> bool:
     if high is not None and high.val <= node.val:
         return False
 
-    return self.validate(node.left, low, node) \
-           and self.validate(node.right, node, high)
+    return validate(node.left, low, node) and validate(node.right, node, high)
 
 
-def isValidBST(self, root) -> bool:
-    return self.validate(root, None, None)
+def isValidBST(root) -> bool:
+    return validate(root, None, None)
+
+
+class ValidateBSTInorder:
+    def __init__(self):
+        self._prev = None
+
+    def validate(self, node):
+        if node is None:
+            return True
+
+        if not self.validate(node.left):
+            return False
+
+        if self._prev is not None and self._prev.val >= node.val:
+            return False
+
+        self._prev = node
+        return self.validate(node.right)
+
+    def isValidBSTInorder(self, root) -> bool:
+        return self.validate(root)
